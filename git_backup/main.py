008bee12aeb3aace6e7045c548444a81e7507a82
Max Edell <mgedell@gmail.com>
2022-07-26 00:17:07 -0700

    fix: backoff sync in main

--- git_backup/main.py
+++ git_backup/main.py
@@ -1,10 +1,27 @@
 from time import sleep
+from typing import Any, Callable, List
 
 from git_backup.config import load
 from git_backup.sync import sync
 from git_backup.logger import get_logger
 
 log = get_logger('main')
+
+def backoff(fn: Callable, args: List[Any], timeout: int = 60*60, delay: int = 100):
+    attempt = 0
+    timer = 0
+    while timer < timeout:
+        try:  
+            return fn(*args)
+        except Exception as e:
+            log.error(f'ERROR: back_off({attempt}) Unhandled exception: \n{e}')
+            attempt += 1
+            delay *= 2
+            timer += delay
+            if timer > timeout:
+                break
+            sleep(delay)
+    log.error(f'ERROR: timeout while attempting to backoff call')
 
 def run():
     log.info('starting up')
@@ -15,7 +32,7 @@ def run():
     
     if not loop['loop']:
         log.info('running single sync')
-        sync(conf)
+        backoff(sync, [conf])
     else:
         scheduled = 'schedule' in loop
         if scheduled:
@@ -24,9 +41,8 @@ def run():
             log.info(f'running in loop every {loop["interval"]} minutes')
             
         while True:   
-            log.debug('start sync')      
-            sync(conf)
-            
+            log.debug('start sync')
+            backoff(sync, [conf])
             if scheduled:
                 sleep(loop['schedule'].next())
             else:
